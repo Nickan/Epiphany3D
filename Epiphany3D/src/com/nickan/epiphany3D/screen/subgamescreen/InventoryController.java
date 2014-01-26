5e6548840218f257f0828921ce58c245347a2345
Nickan <colete_nico@yahoo.com>
2014-01-26 19:51:32 +0800

    Configure the add attribute buttons

--- Epiphany3D/src/com/nickan/epiphany3D/screen/subgamescreen/InventoryController.java
+++ Epiphany3D/src/com/nickan/epiphany3D/screen/subgamescreen/InventoryController.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.nickan.epiphany3D.model.StatisticsHandler;
 import com.nickan.epiphany3D.model.items.Inventory;
 
 public class InventoryController {
@@ -116,6 +117,12 @@ public class InventoryController {
 	private void initializeAddButtons() {
 		screen.addButtonStr.addListener(new InputListener() {
 			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+				StatisticsHandler statsHandler = screen.gameScreen.world.player.statsHandler;
+				if (statsHandler.remainingStatusPoints > 0) {
+					statsHandler.addBaseStr();
+				} else {
+					screen.stage.getActors().removeAll(screen.addAttributeButtons, true);
+				}
 				return true;
 			}
 
@@ -125,6 +132,12 @@ public class InventoryController {
 		
 		screen.addButtonDex.addListener(new InputListener() {
 			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+				StatisticsHandler statsHandler = screen.gameScreen.world.player.statsHandler;
+				if (statsHandler.remainingStatusPoints > 0) {
+					statsHandler.addBaseDex();
+				} else {
+					screen.stage.getActors().removeAll(screen.addAttributeButtons, true);
+				}
 				return true;
 			}
 
@@ -134,6 +147,12 @@ public class InventoryController {
 		
 		screen.addButtonVit.addListener(new InputListener() {
 			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+				StatisticsHandler statsHandler = screen.gameScreen.world.player.statsHandler;
+				if (statsHandler.remainingStatusPoints > 0) {
+					statsHandler.addBaseVit();
+				} else {
+					screen.stage.getActors().removeAll(screen.addAttributeButtons, true);
+				}
 				return true;
 			}
 
@@ -143,6 +162,12 @@ public class InventoryController {
 		
 		screen.addButtonAgi.addListener(new InputListener() {
 			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+				StatisticsHandler statsHandler = screen.gameScreen.world.player.statsHandler;
+				if (statsHandler.remainingStatusPoints > 0) {
+					statsHandler.addBaseAgi();
+				} else {
+					screen.stage.getActors().removeAll(screen.addAttributeButtons, true);
+				}
 				return true;
 			}
 
@@ -152,13 +177,18 @@ public class InventoryController {
 		
 		screen.addButtonWis.addListener(new InputListener() {
 			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+				StatisticsHandler statsHandler = screen.gameScreen.world.player.statsHandler;
+				if (statsHandler.remainingStatusPoints > 0) {
+					statsHandler.addBaseWis();
+				} else {
+					screen.stage.getActors().removeAll(screen.addAttributeButtons, true);
+				}
 				return true;
 			}
 
 			public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
 			}
 		});
-		
 	}
 	
 	private void initializeResumeButton() {
