9c87f939e779163949dae7d83d97cfa451894358
Nickan <colete_nico@yahoo.com>
2014-01-26 10:59:17 +0800

    Added Inventory

--- Epiphany3D/src/com/nickan/epiphany3D/model/Player.java
+++ Epiphany3D/src/com/nickan/epiphany3D/model/Player.java
@@ -1,16 +1,19 @@
 package com.nickan.epiphany3D.model;
 
 import com.badlogic.gdx.math.Vector3;
+import com.nickan.epiphany3D.model.items.Inventory;
 
 public class Player extends Character {
+	public Inventory inventory;
 
 	public Player(Vector3 position, Vector3 rotation, Vector3 velocity,
 			float speed) {
 		super(position, rotation, velocity, speed);
-		// TODO Auto-generated constructor stub
+		inventory = new Inventory(statsHandler);
 	}
 
 	public void update(float delta) {
+		inventory.update(delta);
 		super.update(delta);
 	}
 
