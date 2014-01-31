8b7a8442ca5e7f4b22a54f5bfa8386523fd28df2
Nickan <colete_nico@yahoo.com>
2014-02-01 00:12:25 +0800

    Added more randomness

--- Epiphany3D/src/com/nickan/framework1_0/math/RandomNumber.java
+++ Epiphany3D/src/com/nickan/framework1_0/math/RandomNumber.java
@@ -3,12 +3,13 @@ package com.nickan.framework1_0.math;
 import java.util.Random;
 
 /**
- * I don't need to have multiple random number creator, so I did this class
+ * I don't need to have multiple random number creator, so I made this class
  * @author Nickan
  *
  */
 public class RandomNumber {
 	private static Random random = new Random();
+	private static long randomizer = 0;
 
 	private RandomNumber() { }
 
@@ -19,7 +20,8 @@ public class RandomNumber {
 	 * @return - The values between min and max
 	 */
 	public static int getRandomInt(int min, int max) {
-		random.setSeed(System.currentTimeMillis());
+		++randomizer;
+		random.setSeed(System.currentTimeMillis() + randomizer);
 		int randomNumber = random.nextInt(max - min);
 		return randomNumber + min + 1;
 	}
